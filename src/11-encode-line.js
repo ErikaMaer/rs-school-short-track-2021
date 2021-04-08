3d6429fa8ae39e568f8970f6583de7c222eb7fd7
Erika <erikamaer00@gmail.com>
2021-04-09 02:48:55 +0300

    feat: add 11 task solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let a = 1;
+  const arr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      a++;
+    } else {
+      arr.push(a + str[i]);
+      a = 1;
+    }
+  }
+  return arr.join('').replace(/1/g, '');
 }
 
 module.exports = encodeLine;
