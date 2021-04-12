0e2f02601ea66885d2d9a59b69f9b2fa3ee5fe59
Erika <erikamaer00@gmail.com>
2021-04-12 15:06:57 +0300

    feat: add 4 task solution

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,45 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  function counter(array, i, j) {
+    let a = 0;
+
+    const prev = array[i - 1];
+    const curr = array[i];
+    const next = array[i + 1];
+
+    [prev, curr, next].forEach((row) => {
+      if (row) {
+        if (row[j - 1] === true) {
+          a++;
+        }
+        if (row[j] === true) {
+          a++;
+        }
+        if (row[j + 1] === true) {
+          a++;
+        }
+      }
+    });
+    return a;
+  }
+
+  const arr = [];
+  const a = matrix.map((x, i) => x.map((b, j) => {
+    if (b === true) {
+      arr.push(counter(matrix, i, j) - 1);
+      return true;
+    }
+    return counter(matrix, i, j);
+  }));
+  const b = a.map((x) => x.map((y) => {
+    if (y === true) {
+      return arr.shift();
+    }
+    return y;
+  }));
+  return b;
 }
 
 module.exports = minesweeper;
